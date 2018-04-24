a1e4d8daeaa40e54b15e4b31683c8fe9fbf8274a
Calvin <calvin@Calvins-MacBook-Pro.local>
2018-04-24 13:28:41 -0700

    open shift added

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -21,6 +23,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//OpenShift
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
