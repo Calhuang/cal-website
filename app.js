1a6b4ff637e7957e54b7393af39e5ec0b8e1328d
Calvin <calvin@Calvins-MacBook-Pro.local>
2018-04-24 15:07:33 -0700

    cahnged ports again

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.PORT || 3000;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 var app = express();
 
